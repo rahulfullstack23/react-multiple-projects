4308c814c6934f87bed17a2cc3bef2172a028056
Rahul Kumar <hmko5432@gmail.com>
2026-09-22 16:43:57 +0530

    product

---
+++ Product/src/App.jsx
@@ -0,0 +1,36 @@
+const App = () => {
+  const product = [
+    {
+      id: 1,
+      name: "laptop",
+      price: 30000,
+      category: "Electronics",
+    },
+
+    {
+      id: 2,
+      name: "Mobile",
+      price: 25000,
+      category: "Electronics",
+    },
+    {
+      id: 3,
+      name: "shoes",
+      price: 3000,
+      category: "fashion",
+    },
+  ];
+  return (
+    <div>
+      <h1>Product List</h1>
+      {product.map((productList) => (
+        <div key={productList.id}>
+          {productList.name}
+          {productList.price} {productList.category}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
