7a5b0f6023345784695d11c48a2f37315b93b99c
Rahul Kumar <hmko5432@gmail.com>
2026-09-26 18:37:29 +0530

    redux toolkit

---
+++ reduxToolkit/src/App.jsx
@@ -0,0 +1,13 @@
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+const App = () => {
+  return (
+    <div>
+      <h1>learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </div>
+  )
+}
+
+export default App
---
+++ reduxToolkit/src/app/store.js
@@ -0,0 +1,7 @@
+import todoReducer from '../features/todo/todoSlice';
+import { configureStore } from '@reduxjs/toolkit';
+export const store=configureStore({
+    reducer: todoReducer
+});
+
+
